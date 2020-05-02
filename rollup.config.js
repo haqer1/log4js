b16ed7dc85c78948d049cc74984b8cc9476085f5
Reşat SABIQ <haqer@gmx.fr>
2020-05-02 22:45:16 +0600

    Add rollup-plugin-node-builtins dependency (package.json), & import it as a plugin in rollup.config.js (for util.format(...) support in prior commit)

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { uglify } from "rollup-plugin-uglify";
+import nodeBuiltins from "rollup-plugin-node-builtins";
 
 const ARTIFACT_NAME = "log4js";
 export default {
     input: "src/main/js/index.js",
+    external: ["console"],
     output: [
       {
+        globals: { "util": "util" },
         file: "dist/" +ARTIFACT_NAME+ ".js",
         format: "umd",
         name: ARTIFACT_NAME
       },
       {
+        globals: { "util": "util" },
         file: "dist/" +ARTIFACT_NAME+ ".min.js",
         format: "umd",
         name: ARTIFACT_NAME,
         plugins: [uglify()]
       }
     ],
-    plugins: [commonjs()]
+    plugins: [commonjs(), nodeBuiltins()]
 };
