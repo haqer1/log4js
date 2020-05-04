2214a0270d7f6696c23c51891ce664b01e47a84f
Reşat SABIQ <haqer@gmx.fr>
2020-05-04 22:10:27 +0600

    Add -for-browsers build artifacts without formatting code imported from Node.js util module (resulting .min.js is 4.8KB (35%) smaller)
     - add rollup-plugin-re dependency (package.json)
     - use it in rollup.config.js to make a build having replaced the relevant import with a comment

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,21 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { uglify } from "rollup-plugin-uglify";
 import nodeBuiltins from "rollup-plugin-node-builtins";
+import replace from 'rollup-plugin-re';
+//import externalGlobals from "rollup-plugin-external-globals";
+//import ignoreImport from 'rollup-plugin-ignore-import';
 
 const ARTIFACT_NAME = "log4js";
-export default {
+export default [
+{
     input: "src/main/js/index.js",
-    external: ["console"],
     output: [
       {
-        globals: { "util": "util" },
         file: "dist/" +ARTIFACT_NAME+ ".js",
         format: "umd",
         name: ARTIFACT_NAME
       },
       {
-        globals: { "util": "util" },
         file: "dist/" +ARTIFACT_NAME+ ".min.js",
         format: "umd",
         name: ARTIFACT_NAME,
@@ -22,4 +23,59 @@ export default {
       }
     ],
     plugins: [commonjs(), nodeBuiltins()]
-};
+},
+{
+    input: "src/main/js/index.js",
+    output: [
+      {
+        file: "dist/" +ARTIFACT_NAME+ "-for-browsers.js",
+        format: "umd",
+        name: ARTIFACT_NAME
+      },
+      {
+        file: "dist/" +ARTIFACT_NAME+ "-for-browsers.min.js",
+        format: "umd",
+        name: ARTIFACT_NAME,
+        plugins: [uglify()]
+      }
+    ],
+    plugins: [
+        replace({ // seems better than auto-generating a couple of files, etc.
+            replaces: {
+//                "import {format} from \"util\";": "//deleted"
+            },
+            patterns:[
+                {
+                    include: "src/main/js/com/adazes/util/log4js/Logger.js",
+                    test: /import \{ ?format ?\} from ["']util["'];/g,
+                    replace: "// not using Node.js util module for browsers"
+                }
+            ]
+        })
+    ]
+}
+/* Works partially: leaves util.format while util is not defined (only function is imported, & importing everything would be wasteful)
+{
+    input: "src/main/js/index.js",
+    output: [
+      {
+        file: "dist/" +ARTIFACT_NAME+ "-for-browsers.2.js",
+        format: "umd",
+        name: ARTIFACT_NAME
+      }
+    ],
+    plugins: [externalGlobals({util:"util"})]
+},
+{
+    input: "src/main/js/index.js",
+    output: [
+      {
+        file: "dist/" +ARTIFACT_NAME+ "-for-browsers.2.min.js",
+        format: "umd",
+        name: ARTIFACT_NAME
+      }
+    ],
+    plugins: [externalGlobals({util:"util"}),uglify()]
+}*/
+]
+;
